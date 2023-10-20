8dc8d6b3552dd8e271a8887e92b015ce7461e62d
Arshlankhan786 <141414515+Arshlankhan786@users.noreply.github.com>
2023-10-20 16:12:07 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -1,7 +1,7 @@
-# Copyright (C) 2023 DX_MODS
+# Copyright (C) 2023 Pathan_botz
 #Licensed under the  AGPL-3.0 License;
 #you may not use this file except in compliance with the License.
-#Author ZIYAN"
+#Author Arshlan"
 
 class mr(object):
     PROGRESS_BAR = """\n
